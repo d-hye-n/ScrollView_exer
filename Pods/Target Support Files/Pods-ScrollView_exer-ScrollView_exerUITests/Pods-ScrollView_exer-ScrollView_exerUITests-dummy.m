25289510b505c41ae14885d72a196260459f14ce
d-hye-n <qordkadkwla@gmail.com>
2025-05-20 13:28:46 +0900

    initial

---
+++ Pods/Target Support Files/Pods-ScrollView_exer-ScrollView_exerUITests/Pods-ScrollView_exer-ScrollView_exerUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ScrollView_exer_ScrollView_exerUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ScrollView_exer_ScrollView_exerUITests
+@end
