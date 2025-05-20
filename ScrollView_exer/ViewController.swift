64a83184a2b38cf6fc54cbf3fd84201b82e3fb6a
d-hye-n <qordkadkwla@gmail.com>
2025-05-20 13:20:10 +0900

    Initial Commit

---
+++ ScrollView_exer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrollView_exer
+//
+//  Created by 김도현 on 5/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ScrollView_exerTests/ScrollView_exerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollView_exerTests.swift
+//  ScrollView_exerTests
+//
+//  Created by 김도현 on 5/20/25.
+//
+
+import Testing
+@testable import ScrollView_exer
+
+struct ScrollView_exerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
